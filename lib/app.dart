3053dc963295cde44cf44fb62e3463a6d235c0dc
kotako <sy.empress0321@gmail.com>
2018-09-22 15:21:11 +0900

    First commit

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Lemonade extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Lemonade',
+        home: Scaffold(),
+      );
+}
